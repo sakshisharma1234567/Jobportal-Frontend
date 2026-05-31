ae155d8f90feab211781057efeaba934e9d92bb6
sakshisharma1234567 <sakshu9650@gmail.com>
2026-05-31 23:11:43 +0530

    Change API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://jobportal-backend-1-6y9y.onrender.com/api",
 });
 
 export default API;
