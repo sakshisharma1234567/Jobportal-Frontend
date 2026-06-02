d9d6ebb15b90aa61fe3daa63824cf16e155a648a
sakshisharma1234567 <sakshu9650@gmail.com>
2026-06-02 15:14:20 +0530

    Refactor ResumeViewer to Register component

--- src/pages/ResumeViewer.jsx
+++ src/pages/ResumeViewer.jsx
@@ -1,31 +1,98 @@
-import { useLocation } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
+import { useState } from "react";
+import API from "../services/api";
+import { Link, useNavigate } from "react-router-dom";
 
-function ResumeViewer() {
-  const location = useLocation();
+function Register() {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "student",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.post("/auth/register", form);
+
+      alert("Registration Successful");
+
+      navigate("/");
+    } catch (error) {
+      alert(error.response?.data?.message || "Something went wrong");
+    }
+  };
 
   return (
-    <div style={{ display: "flex" }}>
-      <Sidebar />
+    <div className="container">
+      <div className="card">
+        <h1>Register</h1>
 
-      <div
-        style={{
-          flex: 1,
-          padding: "20px",
-        }}
-      >
-        <iframe
-          src={location.state?.resume}
-          width="100%"
-          height="900px"
-          title="Resume"
-          style={{
-            border: "none",
-          }}
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            onChange={handleChange}
+            required
+          />
+
+          <br />
+          <br />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+            required
+          />
+
+          <br />
+          <br />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+            required
+          />
+
+          <br />
+          <br />
+
+          <select name="role" onChange={handleChange}>
+            <option value="student">Student</option>
+
+            <option value="recruiter">Recruiter</option>
+          </select>
+
+          <br />
+          <br />
+
+          <button type="submit">Register</button>
+        </form>
+
+        <br />
+
+        <p>
+          Already have an account?
+          <Link to="/"> Login</Link>
+        </p>
       </div>
     </div>
   );
 }
 
-export default ResumeViewer;
+export default Register;
